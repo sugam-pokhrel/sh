9e92a654a7be186b3a8ad9facf8d4e6495161129
sugam-pokhrel <114973396+sugam-pokhrel@users.noreply.github.com>
2022-11-04 17:50:08 +0545

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,20 @@
-apt update -y && apt install wget -y && apt install curl -y && apt-get install software-properties-common && apt update -y  && apt install  python3 pip -y && apt update -y && apt-get install -y gnupg2 gnupg gnupg1   && apt update -y &&  apt-get install xvfb -y &&  apt-get install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic -y && apt update -y && export DISPLAY=:1   &&  Xvfb :1 -screen 0 1920x1080x24 & && apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F -y && apt update -y && apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu focal main" -y && apt update -y && apt install firefox -y && curl -sSL https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz | tar -xvz && chmod +x geckodriver &&  mv geckodriver /usr/local/bin/   
+apt update -y 
+apt install wget -y 
+apt install curl -y 
+apt-get install software-properties-common 
+apt update -y 
+apt install  python3 pip -y 
+apt update -y 
+apt-get install -y gnupg2 gnupg gnupg1   
+apt update -y &&  apt-get install xvfb -y 
+apt-get install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic -y 
+apt update -y 
+export DISPLAY=:1   
+Xvfb :1 -screen 0 1920x1080x24 & 
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F -y 
+apt update -y 
+apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu focal main" -y 
+apt update -y 
+apt install firefox -y 
+curl -sSL https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz | tar -xvz && chmod +x geckodriver 
+mv geckodriver /usr/local/bin/   
