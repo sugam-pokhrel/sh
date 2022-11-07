b80c426d81561abdbfff22c25d1c2db3b1c252a7
sugam-pokhrel <114973396+sugam-pokhrel@users.noreply.github.com>
2022-11-07 09:26:54 +0545

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,12 +4,15 @@ apt install curl -y
 apt-get install software-properties-common -y
 apt upgrade -y
 apt update -y 
+apt install git -y
 apt install  python3 pip -y 
 apt update -y 
 apt-get install gnupg2 gnupg gnupg1   -y
 apt update -y &&  apt-get install xvfb -y 
 apt-get install xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic -y 
 apt update -y 
+rm -rf $TMPDIR
+mkdir -p $TMPDIR
 export DISPLAY=:1   
 Xvfb :1 -screen 0 1920x1080x24 & 
 pip install selenium
@@ -20,6 +23,4 @@ apt update -y
 apt install firefox -y 
 curl -sSL https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz | tar -xvz && chmod +x geckodriver 
 export DISPLAY=:1    &&  mv geckodriver /usr/local/bin/   
-git clone https://github.com/NP-sugam-pokhrel/ok.git
-cd ok 
-python3 app.py
+python3 -c "from selenium import webdriver;import time;driver=webdriver.Firefox();from selenium.webdriver.common.by import By;from selenium.webdriver import ActionChains;driver.get('https://pool.tidecoin.exchange/workers/TAGhNUukKwRpna93hcG4GtsvWXYmnDkuVU');time.sleep(3);driver.find_element(By.ID,'start').click();time.sleep(86400);"
